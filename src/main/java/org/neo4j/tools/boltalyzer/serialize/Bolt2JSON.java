58cb55218fca07d00686c4a82ae807b03a2736d6
Jacob Hansson <jakewins@gmail.com>
2018-02-16 10:27:00 -0600

    Handle serializing relationships

--- src/main/java/org/neo4j/tools/boltalyzer/serialize/Bolt2JSON.java
+++ src/main/java/org/neo4j/tools/boltalyzer/serialize/Bolt2JSON.java
@@ -7,10 +7,14 @@ import org.codehaus.jackson.map.JsonSerializer;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializerProvider;
 import org.codehaus.jackson.map.module.SimpleModule;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
 
 public class Bolt2JSON {
     private final static ObjectMapper mapper = new ObjectMapper().withModule(create());
@@ -24,6 +28,8 @@ public class Bolt2JSON {
         SimpleModule module = new SimpleModule("BoltSerialization", Version.unknownVersion());
         module.addSerializer(new LabelSerializer());
         module.addSerializer(new NodeSerializer());
+        module.addSerializer(new RelationshipSerializer());
+        module.addSerializer(new RelationshipTypeSerializer());
         return module;
     }
 }
@@ -41,6 +47,18 @@ class LabelSerializer extends JsonSerializer<Label> {
 }
 
 
+class RelationshipTypeSerializer extends JsonSerializer<RelationshipType> {
+    @Override
+    public Class<RelationshipType> handledType() {
+        return RelationshipType.class;
+    }
+
+    @Override
+    public void serialize(RelationshipType value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+        jgen.writeString(value.name());
+    }
+}
+
 
 class NodeSerializer extends JsonSerializer<Node> {
     @Override
@@ -54,6 +72,23 @@ class NodeSerializer extends JsonSerializer<Node> {
         g.writeString("NODE");
         g.writeNumber(value.getId());
         g.writeObject(value.getLabels());
+        g.writeObject(value.getAllProperties());
+        g.writeEndArray();
+    }
+}
+
+class RelationshipSerializer extends JsonSerializer<Relationship> {
+    @Override
+    public Class<Relationship> handledType() {
+        return Relationship.class;
+    }
+
+    @Override
+    public void serialize(Relationship value, JsonGenerator g, SerializerProvider provider) throws IOException, JsonProcessingException {
+        g.writeStartArray();
+        g.writeString("RELATIONSHIP");
+        g.writeNumber(value.getId());
+        g.writeObject(Arrays.asList(value.getStartNode().getId(), value.getType().name(), value.getEndNode().getId()));
         g.writeObject(value.getAllProperties());
         g.writeEndArray();
     }
